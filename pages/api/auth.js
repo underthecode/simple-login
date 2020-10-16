2ef5ffebf8b9817c918508e176f4d4ff0bfd3ed2
Steven <steven@ip-10-0-0-18.ec2.internal>
2020-10-16 12:39:28 -0700

    return Promise

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -21,57 +21,59 @@ const authUser = (password, hash, callback) => {
 };
 
 export default (req, res) => {
-  if (req.method === 'POST') {
-    try {
-      client.connect(() => {
-        console.log('MongoDB connected');
-        const db = client.db(dbName);
-        const email = req.body.email;
-        const password = req.body.password;
+  return new Promise(() => {
+    if (req.method === 'POST') {
+      try {
+        client.connect(() => {
+          console.log('MongoDB connected');
+          const db = client.db(dbName);
+          const email = req.body.email;
+          const password = req.body.password;
 
-        findUser(db, email, (err, user) => {
-          if (err) {
-            res
-              .status(500)
-              .json({ error: true, message: 'Error finding user' });
-            return;
-          }
-          if (!user) {
-            res
-              .status(404)
-              .json({ error: true, message: 'Please provide a valid email' });
-            return;
-          } else {
-            authUser(password, user.password, (err, match) => {
-              if (err) {
-                res
-                  .status(500)
-                  .json({ error: true, message: 'Error finding user' });
-              }
-              if (match) {
-                const token = jwt.sign(
-                  { userId: user.userId, email: user.email },
-                  jwtSecret,
-                  {
-                    expiresIn: 3000
-                  }
-                );
-                res.status(200).json({ token });
-                return;
-              } else {
-                res.status(401).json({
-                  error: true,
-                  message: 'Please provide a valid email'
-                });
-              }
-            });
-          }
+          findUser(db, email, (err, user) => {
+            if (err) {
+              res
+                .status(500)
+                .json({ error: true, message: 'Error finding user' });
+              return;
+            }
+            if (!user) {
+              res
+                .status(404)
+                .json({ error: true, message: 'Please provide a valid email' });
+              return;
+            } else {
+              authUser(password, user.password, (err, match) => {
+                if (err) {
+                  res
+                    .status(500)
+                    .json({ error: true, message: 'Error finding user' });
+                }
+                if (match) {
+                  const token = jwt.sign(
+                    { userId: user.userId, email: user.email },
+                    jwtSecret,
+                    {
+                      expiresIn: 3000
+                    }
+                  );
+                  res.status(200).json({ token });
+                  return;
+                } else {
+                  res.status(401).json({
+                    error: true,
+                    message: 'Please provide a valid email'
+                  });
+                }
+              });
+            }
+          });
         });
-      });
-    } catch (err) {
-      res.status(403).send(err);
+      } catch (err) {
+        res.status(403).send(err);
+      }
+    } else {
+      res.status(401).end();
     }
-  } else {
-    res.status(401).end();
-  }
+  });
 };
--- pages/api/users.js
+++ pages/api/users.js
@@ -35,46 +35,48 @@ const createUser = (db, email, password, callback) => {
 };
 
 export default (req, res) => {
-  if (req.method === 'POST') {
-    try {
-      client.connect(() => {
-        console.log('MongoDB connected');
-        const db = client.db(dbName);
-        const email = req.body.email;
-        const password = req.body.password;
+  return new Promise(() => {
+    if (req.method === 'POST') {
+      try {
+        client.connect(() => {
+          console.log('MongoDB connected');
+          const db = client.db(dbName);
+          const email = req.body.email;
+          const password = req.body.password;
 
-        findUser(db, email, (err, user) => {
-          if (err) {
-            res
-              .status(500)
-              .json({ error: true, message: 'Error finding user' });
-            return;
-          }
-          if (!user) {
-            createUser(db, email, password, (creationResult) => {
-              if (creationResult.ops.length === 1) {
-                const user = creationResult.ops[0];
-                const token = jwt.sign(
-                  { userId: user.userId, email: user.email },
-                  jwtSecret,
-                  {
-                    expiresIn: 3000
-                  }
-                );
-                res.status(200).json({ token });
-                return;
-              }
-            });
-          } else {
-            res.status(403).json({ error: true, message: 'User exists' });
-            return;
-          }
+          findUser(db, email, (err, user) => {
+            if (err) {
+              res
+                .status(500)
+                .json({ error: true, message: 'Error finding user' });
+              return;
+            }
+            if (!user) {
+              createUser(db, email, password, (creationResult) => {
+                if (creationResult.ops.length === 1) {
+                  const user = creationResult.ops[0];
+                  const token = jwt.sign(
+                    { userId: user.userId, email: user.email },
+                    jwtSecret,
+                    {
+                      expiresIn: 3000
+                    }
+                  );
+                  res.status(200).json({ token });
+                  return;
+                }
+              });
+            } else {
+              res.status(403).json({ error: true, message: 'User exists' });
+              return;
+            }
+          });
         });
-      });
-    } catch (err) {
-      res.status(403).json({ error: true, message: err.message });
+      } catch (err) {
+        res.status(403).json({ error: true, message: err.message });
+      }
+    } else {
+      res.status(200).json({ users: ['John Doe'] });
     }
-  } else {
-    res.status(200).json({ users: ['John Doe'] });
-  }
+  });
 };
