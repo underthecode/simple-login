ab06d57dcc3abb2bf0cbddeecf66b33b25dce347
Steven <steven@ip-10-0-0-18.ec2.internal>
2020-10-15 18:54:32 -0700

    init signup api

---
+++ pages/api/users.js
@@ -0,0 +1,80 @@
+const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcrypt');
+const v4 = require('uuid').v4;
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+
+const saltRounds = 10;
+const url = 'mongodb://localhost:27017';
+const dbName = 'simple-app';
+
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const findUser = (db, email, callback) => {
+  const collection = db.collection('user');
+  collection.findOne({ email }, callback);
+};
+
+const createUser = (db, email, password, callback) => {
+  const collection = db.collection('user');
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    collection.insertOne(
+      {
+        userId: v4(),
+        email,
+        password: hash
+      },
+      (err, userCreated) => {
+        callback(userCreated);
+      }
+    );
+  });
+};
+
+export default (req, res) => {
+  if (req.method === 'POST') {
+    try {
+      client.connect(() => {
+        console.log('MongoDB connected');
+        const db = client.db(dbName);
+        const email = req.body.email;
+        const password = req.body.password;
+
+        findUser(db, email, (err, user) => {
+          if (err) {
+            res
+              .status(500)
+              .json({ error: true, message: 'Error finding user' });
+            return;
+          }
+          if (!user) {
+            createUser(db, email, password, (creationResult) => {
+              if (creationResult.ops.length === 1) {
+                const user = creationResult.ops[0];
+                const token = jwt.sign(
+                  { userId: user.userId, email: user.email },
+                  jwtSecret,
+                  {
+                    expiresIn: 3000
+                  }
+                );
+                res.status(200).json({ token });
+                return;
+              }
+            });
+          } else {
+            res.status(403).json({ error: true, message: 'User exists' });
+            return;
+          }
+        });
+      });
+    } catch (err) {
+      res.status(403).json({ error: true, message: err.message });
+    }
+  } else {
+    res.status(200).json({ users: ['John Doe'] });
+  }
+};
